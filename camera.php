bb6803b840224131285a38ece0ffd00de393314e
Matthew Heick <mjheick@users.noreply.github.com>
2020-11-20 10:11:47 -0500

    Create camera.php

---
+++ camera.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Take a Video!</title>
+	</head>
+	<body>
+
+<video id="video" width="480" height="640" autoplay></video>
+<button id="snap">Snap Photo</button>
+<canvas id="canvas" width="480" height="640"></canvas>
+
+	</body>
+
+<script>
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+console.log('navigator.mediaDevices:');
+console.log(navigator.mediaDevices);
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+/* Legacy code below: getUserMedia 
+else if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({ video: true }, function(stream) {
+        video.src = stream;
+        video.play();
+    }, errBack);
+} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia({ video: true }, function(stream){
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+    navigator.mozGetUserMedia({ video: true }, function(stream){
+        video.srcObject = stream;
+        video.play();
+    }, errBack);
+}
+*/
+
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 480, 640);
+});
+</script>
+</html>
